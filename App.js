ae21d9b9171cd2c63fd5a5ac5dbe4475aa756128
kevin <kevinmacash@gmail.com>
2026-01-19 17:54:46 -0500

    adding calculador React Native

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [numero1, setNumero1] = useState(0);
+  const [numero2, setNumero2] = useState(0);
+  const [resultado, setResultado] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Calculadora </Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.textInput}
+        value={numero1}
+        placeholder="Ingrese el primer numero"
+        onChangeText={(txt) => {
+          setNumero1(parseInt(txt));
+        }}
+      ></TextInput>
+      <TextInput
+        style={styles.textInput}
+        value={numero2}
+        placeholder="Ingrese el segundo numero"
+        onChangeText={(txt) => {
+          setNumero2(parseInt(txt));
+        }}
+      ></TextInput>
+      <View
+        style={{
+          height: 20,
+        }}
+      ></View>
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <Button
+          title="Sumar"
+          onPress={() => {
+            setResultado(numero1 + numero2);
+          }}
+        ></Button>
+
+        <Button
+          title="Restar"
+          onPress={() => {
+            setResultado(numero1 - numero2);
+          }}
+        ></Button>
+        <Button
+          title="Multiplicar"
+          onPress={() => {
+            setResultado(numero1 * numero2);
+          }}
+        ></Button>
+      </View>
+      <Text>Resultado: {resultado}</Text>
     </View>
   );
 }
@@ -13,8 +60,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    paddingTop: 5,
+    paddingHorizontal: 10,
+    width: 200,
   },
 });
